ce65a38949090b8d90e27ed63acec0109cb8bf31
petual1 <danielalexandre202@hotmail.com>
2023-12-02 14:10:51 -0300

    error

--- projeto_siom/deployment.py
+++ projeto_siom/deployment.py
@@ -2,7 +2,7 @@ import os
 from .settings import *
 from .settings import BASE_DIR
 
-SECRET_KEY = os.environ['SECRET']
+SECRET_KEY = os.environ['SECRET_KEY']
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
 CSRF_TRUSTED_ORIGINS = ['https://'+os.environ['WEBSITE_HOSTNAME']]
 DEBUG = False
